4a7bc700b76f8389ba54bb06e30d5f56e50668eb
Juan Diego Ocampo <jd.ocampom0117@icloud.com>
2020-12-23 14:55:05 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Juan Diego Ocampo on 23/12/2020.
+//  Copyright ©2020 Juan Diego Ocampo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK:- Variables
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    // MARK:- Calculator Methods
+    
+    /// Tag: calcButtonPressed()
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        // What should happen when a non-number button is pressed
+        
+    }
+    
+    /// Tag: numButtonPressed()
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        // What should happen when a number is entered into the keypad
+        
+    }
+    
+}
+
